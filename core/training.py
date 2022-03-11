ca52d918299280a6a1637de2f9a6ac6a6d201dd6
Martin Marek <martin.marek1999@gmail.com>
2022-03-11 15:37:35 +0100

    bug fixes

--- core/training.py
+++ core/training.py
@@ -27,7 +27,7 @@ def vmap_over_keys(f, key, n):
 def create_sgd_chains(key, log_likelihood_fn, params_init, init_sd, n_epochs, ll_start, ll_stop, n_chains, n_samples):
     def create_chain(key):
         init_key, mcmc_key = jax.random.split(key, 2)
-        params = initialize_params(key, params_init, init_sd)
+        params = initialize_params(init_key, params_init, init_sd)
         params, loss_history = train_sgd(params, log_likelihood_fn, n_epochs, ll_start, ll_stop)
         return jnp.concatenate([ravel_fn(params), loss_history])
 
@@ -41,7 +41,7 @@ def create_sgd_chains(key, log_likelihood_fn, params_init, init_sd, n_epochs, ll
 def create_rwmh_chains(key, log_likelihood_fn, params_init, init_sd, step_size, n_blind_steps, n_chains, n_samples):
     def create_chain(key):
         init_key, mcmc_key = jax.random.split(key, 2)
-        params = initialize_params(key, params_init, init_sd)
+        params = initialize_params(init_key, params_init, init_sd)
         chain, avg_accept_prob = rwmh_sampler(params, log_likelihood_fn, mcmc_key, n_samples, n_blind_steps, step_size)
         return jnp.concatenate([chain_to_arr(chain).flatten(), jnp.array([avg_accept_prob])])
 
@@ -55,7 +55,7 @@ def create_rwmh_chains(key, log_likelihood_fn, params_init, init_sd, step_size, 
 def create_hmc_chains(key, log_likelihood_fn, params_init, init_sd, step_size, n_leapfrog_steps, n_chains, n_samples):
     def create_chain(key):
         init_key, mcmc_key = jax.random.split(key, 2)
-        params = initialize_params(key, params_init, init_sd)
+        params = initialize_params(init_key, params_init, init_sd)
         chain, avg_accept_prob = hmc_sampler(params, log_likelihood_fn, mcmc_key, n_samples, n_leapfrog_steps, step_size)
         return jnp.concatenate([chain_to_arr(chain).flatten(), jnp.array([avg_accept_prob])])
 
@@ -69,7 +69,7 @@ def create_hmc_chains(key, log_likelihood_fn, params_init, init_sd, step_size, n
 def create_mixed_chains(key, log_likelihood_fn, params_init, init_sd, n_epochs, sgd_ll_start, sgd_ll_stop, step_size, n_leapfrog_steps, n_chains, n_outer_steps, n_inner_steps):
     def create_chain(key):
         init_key, mcmc_key = jax.random.split(key, 2)
-        params = initialize_params(key, params_init, init_sd)
+        params = initialize_params(init_key, params_init, init_sd)
         params, _ = train_sgd(params, log_likelihood_fn, n_epochs, sgd_ll_start, sgd_ll_stop)
         chain, avg_accept_prob = hmc_sampler(params, log_likelihood_fn, mcmc_key, n_inner_steps, n_leapfrog_steps, step_size)
         return jnp.concatenate([chain_to_arr(chain).flatten(), jnp.array([avg_accept_prob])])
@@ -81,18 +81,3 @@ def create_mixed_chains(key, log_likelihood_fn, params_init, init_sd, n_epochs, 
     avg_accept_prob = out[:, -1].mean()
     return chains, avg_accept_prob
 
-
-def create_numpyro_chains(key, log_likelihood_fn, params_init, init_sd, n_epochs, sgd_ll_start, sgd_ll_stop, step_size, n_leapfrog_steps, n_chains, n_outer_steps, n_inner_steps):
-    def create_chain(key):
-        init_key, mcmc_key = jax.random.split(key, 2)
-        params = initialize_params(key, params_init, init_sd)
-        params, _ = train_sgd(params, log_likelihood_fn, n_epochs, sgd_ll_start, sgd_ll_stop)
-        chain, avg_accept_prob = hmc_sampler(params, log_likelihood_fn, mcmc_key, n_inner_steps, n_leapfrog_steps, step_size)
-        return jnp.concatenate([chain_to_arr(chain).flatten(), jnp.array([avg_accept_prob])])
-
-    n_params = len(ravel_fn(params_init))
-    n_samples = n_outer_steps*n_inner_steps
-    out = vmap_over_keys(create_chain, key, n_chains*n_outer_steps)
-    chains = out[:, :-1].reshape([n_chains, n_samples, -1])
-    avg_accept_prob = out[:, -1].mean()
-    return chains, avg_accept_prob
