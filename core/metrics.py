cf6067fdb2f9f99ef56f17035265d340eebd592d
Martin Marek <martin.marek1999@gmail.com>
2022-03-14 19:00:31 +0100

    updated metrics, added calibration

--- core/metrics.py
+++ core/metrics.py
@@ -2,23 +2,19 @@ import jax
 import jax.numpy as jnp
 
 
-def autocorr(chains, f=None, n_lags=20):
+def autocorr(y, n_lags=20):
     """
-    chains: [num_chains x num_steps x num_params]
-    f: f(chain_node) -> [B]: transforms params to a 1D quantity of interest, eg predictive distribution
+    y.shape: [num_chains (M) x num_steps (N) x num_features (B)]
     """
-    
-    # map model parameters to a 1D array of features
-    y = jnp.array([[f(params) for params in chain] for chain in chains])
     M, N, B = y.shape
 
     # a single step in the loop compute autocovariance for a given lag
     def step(i, r):
         a = y[:, n_lags:, :]
         b = jnp.roll(y, i, axis=1)[:, n_lags:, :]
-        cov_ab = ((a - a.mean(axis=[0, 1], keepdims=True)) * (b - b.mean(axis=[0, 1], keepdims=True))).mean(axis=1)
-        var_a = ((a - a.mean(axis=[0, 1], keepdims=True))**2).mean(axis=1)
-        var_b = ((b - a.mean(axis=[0, 1], keepdims=True))**2).mean(axis=1)
+        cov_ab = ((a - a.mean(axis=(0, 1), keepdims=True)) * (b - b.mean(axis=(0, 1), keepdims=True))).mean(axis=1)
+        var_a = ((a - a.mean(axis=(0, 1), keepdims=True))**2).mean(axis=1)
+        var_b = ((b - a.mean(axis=(0, 1), keepdims=True))**2).mean(axis=1)
         cor = cov_ab / jnp.sqrt(var_a*var_b)
         r = r.at[:, i, :].set(cor)
         return r
@@ -34,21 +30,16 @@ def autocorr(chains, f=None, n_lags=20):
     return r
 
 
-def r_hat(chains, f):
+def r_hat(y):
     """
     Based on the paper "What Are Bayesian Neural Network Posteriors Really Like? - Appendix B"
-    chains: [num_chains x num_steps x num_params]
-    f: f(chain_node) -> [B]: transforms params to a 1D quantity of interest, eg predictive distribution
+    y.shape: [num_chains (M) x num_steps (N) x num_features (B)]
     """
-    
-    # compute function of interest for each chain node
-    # - y.shape: [num_chains (M) x num_steps (N) x num_features (B)]
-    y = jnp.array([[f(params) for params in chain] for chain in chains])
     M, N, B = y.shape
 
     # compute variances
-    b = (N/(M-1)) * ((y.mean(axis=1, keepdims=True) - y.mean(axis=[0, 1], keepdims=True))**2).sum(axis=[0, 1])
-    w = 1/(M*(N-1)) * ((y - y.mean(axis=1, keepdims=True))**2).sum(axis=[0, 1])
+    b = (N/(M-1)) * ((y.mean(axis=1, keepdims=True) - y.mean(axis=(0, 1), keepdims=True))**2).sum(axis=(0, 1))
+    w = 1/(M*(N-1)) * ((y - y.mean(axis=1, keepdims=True))**2).sum(axis=(0, 1))
     r_hat = jnp.sqrt((((N-1)/N)*w + (1/N)*b)/w)
     
     return r_hat
