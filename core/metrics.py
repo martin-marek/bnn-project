df636d63d6ad7c4b11d92626da864fdf8315a505
Martin Marek <martin.marek1999@gmail.com>
2022-03-15 20:20:56 +0100

    autocorr works for a single chain now

--- core/metrics.py
+++ core/metrics.py
@@ -4,28 +4,26 @@ import jax.numpy as jnp
 
 def autocorr(y, n_lags=20):
     """
-    y.shape: [num_chains (M) x num_steps (N) x num_features (B)]
+    y.shape: [num_steps (N) x num_features (B)]
     """
-    M, N, B = y.shape
+    N, B = y.shape
 
     # a single step in the loop compute autocovariance for a given lag
     def step(i, r):
-        a = y[:, n_lags:, :]
-        b = jnp.roll(y, i, axis=1)[:, n_lags:, :]
-        cov_ab = ((a - a.mean(axis=(0, 1), keepdims=True)) * (b - b.mean(axis=(0, 1), keepdims=True))).mean(axis=1)
-        var_a = ((a - a.mean(axis=(0, 1), keepdims=True))**2).mean(axis=1)
-        var_b = ((b - a.mean(axis=(0, 1), keepdims=True))**2).mean(axis=1)
-        cor = cov_ab / jnp.sqrt(var_a*var_b)
-        r = r.at[:, i, :].set(cor)
+        a = y[n_lags:, :]
+        b = jnp.roll(y, i, axis=0)[n_lags:, :]
+        cov_ab = ((a - a.mean(axis=0, keepdims=True)) * (b - b.mean(axis=0, keepdims=True))).mean(axis=0)
+        cor = cov_ab / (a.std(axis=0) * b.std(axis=0))
+        r = r.at[i, :].set(cor)
         return r
 
     # compute autocovariance for each chain and feature separately
-    r = jnp.zeros([M, n_lags, B])
-    r = r.at[:, 0, :].set(1)
+    r = jnp.zeros([n_lags, B])
+    r = r.at[0, :].set(1)
     r = jax.lax.fori_loop(1, n_lags, step, r)
 
-    # take a mean along chains and features
-    r = r.mean(axis=[0, 2])
+    # take a mean along features
+    r = r.mean(axis=1)
     
     return r
 
