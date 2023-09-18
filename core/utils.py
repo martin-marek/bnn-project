a38ea1d5bb760e79506c38f9d1db364a9d7041bb
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2023-09-18 16:08:47 +0100

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -3,10 +3,6 @@ import jax.numpy as jnp
 from jax.tree_util import tree_flatten
 
 
-def ifelse(cond, val_true, val_false):
-    return jax.lax.cond(cond, lambda x: x[0], lambda x: x[1], (val_true, val_false))
-
-
 def ravel_pytree_(pytree):
     """Ravels a pytree like `jax.flatten_util.ravel_pytree`
     but doesn't return a function for unraveling."""
